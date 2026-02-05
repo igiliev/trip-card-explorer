d1749409b197f91197549af704d8f844aed03e38
unknown <ivaylogi@outlook.com>
2026-02-05 15:04:48 +0200

    Initially request smaller images for better performance

--- src/hooks/useTrips.js
+++ src/hooks/useTrips.js
@@ -5,13 +5,28 @@ export function useTrips() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const optimizeImageUrl = (url) => {
+    if (!url.includes("images.unsplash.com")) return url;
+
+    const joiner = url.includes("?") ? "&" : "?";
+
+    return `${url}${joiner}w=800&auto=format&fit=crop&q=70`;
+  };
+
   useEffect(() => {
     fetch("/data/data.json")
       .then((r) => {
         if (!r.ok) throw new Error("Data fetching failed!");
         return r.json();
       })
-      .then((data) => setTrips(data.trips))
+      .then((data) => {
+        const optimized = data.trips.map((t) => ({
+          ...t,
+          image: optimizeImageUrl(t.image),
+        }));
+
+        setTrips(optimized);
+     })
       .catch((e) => setError(e.message))
       .finally(() => setLoading(false));
   }, []);
